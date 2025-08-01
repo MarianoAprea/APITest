7877026160f3154ea24b9bd3a29410d59d64adaa
Mariano Aprea <apreamariano@gmail.com>
2025-07-31 22:35:54 -0300

    Update list initialization and enhance item type checking in python.py

--- apitest/python.py
+++ apitest/python.py
@@ -1,6 +1,18 @@
 
 lista=[]
-lista = [1, 2, 3, "hola", True]
+lista = [1, 2, -3, "hola", False]
 lista.append(4)
-
-print(lista)
+i = 0
+while i < len(lista):
+    item = lista[i]
+    if isinstance(item, int) and item > 0:
+        print(f"{item} is an integer greater than 0")
+    elif isinstance(item, str):
+        print(f'"{item}" is a string')
+    elif isinstance(item, bool):
+        print(f"{item} is a boolean value")
+    elif isinstance(item, int) and item < 0:
+        print(f"{item} is an integer less than 0")
+    else:
+        print(f"{item} does not match any condition")
+    i += 1
