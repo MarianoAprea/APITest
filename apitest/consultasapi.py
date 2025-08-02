3b96a70504c7daf5c0d41cfa985b6e15bc17de9c
Mariano Aprea <apreamariano@gmail.com>
2025-08-01 22:29:18 -0300

    Add tests for user creation and deletion in consultasapi.py

--- apitest/consultasapi.py
+++ apitest/consultasapi.py
@@ -26,4 +26,30 @@ def test_get_some_user_ok ():
    assert user["id"] == 1
    assert user["nombre"] == "mariano"
 
-   
+def test_post_some_user_ok ():
+   # Realizar una solicitud DELETE para eliminar un usuario
+   data = {
+     "id": 20,
+     "nombre": "Gabo",
+     "apellido": "Cizo",
+     "edad": 28
+    }
+   response = requests.post('http://127.0.0.1:8000/user', json=data)
+   assert response.status_code == 200
+   user = response.json()
+   assert user["message"] == "Usuario creado"
+   assert user["user"] == "Gabo"
+
+
+
+def test_delete_some_user_ok ():
+   # Realizar una solicitud DELETE para eliminar un usuario
+   response = requests.delete('http://127.0.0.1:8000/user/20')
+   assert response.status_code == 200
+   user = response.json()
+   assert user["message"] == "Usuario eliminado"
+   assert user["user"] == "Gabo"
+   response = requests.get('http://127.0.0.1:8000/user/20')
+   assert response.status_code == 200
+   #user = response.json()
+   #assert user["message"] == "No se ha encontrado el usuario"
