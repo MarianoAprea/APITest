7f6fea0563e065ce852a5506df15107c0b6f2934
Mariano Aprea <apreamariano@gmail.com>
2025-08-11 18:01:10 -0300

    Add allure steps for user-related tests in consultasapi.py

--- apitest/consultasapi.py
+++ apitest/consultasapi.py
@@ -1,11 +1,15 @@
 import requests
+import allure
 
+
+@allure.step("Test GET all users")
 def test_get_users_ok ():
    # Realizar una solicitud GET a una URL
    response = requests.get('http://127.0.0.1:8000/users')
    assert response.status_code == 200
    #assert isinstance(response.json(), list)
 
+@allure.step("Test GET user by ID")
 def test_get_some_user_ok ():
    # Realizar una solicitud GET apara obtener un usuario específico
    response = requests.get('http://127.0.0.1:8000/user/1')
@@ -14,6 +18,7 @@ def test_get_some_user_ok ():
    assert user["id"] == 1
    assert user["nombre"] == "mariano"
 
+@allure.step("Test POST user")
 def test_post_some_user_ok ():
    # Realizar una solicitud DELETE para eliminar un usuario
    data = {
@@ -29,7 +34,7 @@ def test_post_some_user_ok ():
    assert user["user"] == "Gabo"
 
 
-
+@allure.step("Test DELETE user")
 def test_delete_some_user_ok ():
    # Realizar una solicitud DELETE para eliminar un usuario
    response = requests.delete('http://127.0.0.1:8000/user/20')
@@ -42,6 +47,7 @@ def test_delete_some_user_ok ():
    #user = response.json()
    #assert user["message"] == "No se ha encontrado el usuario"
 
+@allure.step("Test PUT user")
 def test_put_some_user_ok ():
    # Realizar una solicitud PUT para actualizar un usuario
    data = {
